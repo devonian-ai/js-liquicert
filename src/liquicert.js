6fd48b7cddb1363d6e692406bc714738011b3adf
Red <aransil@gmail.com>
2024-05-16 22:59:44 -0700

    createAttestation functionality

--- src/liquicert.js
+++ src/liquicert.js
@@ -1,8 +1,12 @@
 const { ethers } = require('ethers');
 
+// const apiURL = "http://localhost:3001"
+const apiURL = "http://liquicert.io"
+
 class Liquicert {
-    constructor(provider) {
-        this.provider = new ethers.providers.Web3Provider(provider); // Wrap the provider, preserves accounts
+    constructor(provider, wallet) {
+        this.provider = provider;
+        this.wallet = wallet;
     }
 
     async uploadData(data) {
@@ -10,9 +14,46 @@ class Liquicert {
     }
 
     async createAttestation(attestationName, dataCID, attestationDescription, trustedBool, contractAddress = 'default_sepolia_contract', network = 'sepolia') {
-        if (!attestationName || !dataCID || !attestationDescription) {
+
+        // Make sure we have all the required inputs
+        if (!attestationName || !dataCID || !attestationDescription || !trustedBool) {
             throw new Error('Missing required parameters for creating an attestation.');
         }
+
+        // Create the JSON object that will be our attestation
+        const attestation = {
+            name: attestationName,
+            CID: dataCID,
+            description: attestationDescription,
+            trusted: trustedBool
+        }
+        const attestation_message = JSON.stringify(attestation);
+
+        try {
+            const signature = await this.wallet.signMessage(attestation_message);
+            const signerAddress = await this.wallet.getAddress();
+            
+            const response = await fetch(apiURL+"/addAttestation", {
+                method: "POST",
+                body: JSON.stringify({
+                    signer_address: signerAddress, 
+                    message: attestation_message, 
+                    signature}),
+                headers: {
+                "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+
+            // Handle errors from the api
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error.message || 'Failed to add attestation.');
+            }
+            
+
+        }catch (error) {
+            console.error("Error: "+ error.message)
+        };
         
         console.log('attesting')
     }
